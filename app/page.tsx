b2ca4b51e118d121ed7fe6eac3af88a8c1eca326
Tri Sasongko <tugasspakun@gmail.com>
2025-01-02 15:03:14 +0700

    tri: merubah text menjadi tebal

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="text-center">
-      <h1>Selamat Datang di Katalog Motor</h1>
+      <h1 className="font-bold">Selamat Datang di Katalog Motor</h1>
     </div>
   );
 }
