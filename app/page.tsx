f6313204263c0a526012ccec9f2e459d5c8dee48
Tri Sasongko <tugasspakun@gmail.com>
2025-01-02 14:58:28 +0700

    tri: mengatur text ke tengah

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 export default function Home() {
   return (
-    <h1>Selamat Datang di Katalog Motor</h1>
+    <div className="text-center">
+      <h1>Selamat Datang di Katalog Motor</h1>
+    </div>
   );
 }
