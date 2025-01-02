4f3f6b439d76ba4c054d5d2bdc79e25bedfc3d22
Tri Sasongko <tugasspakun@gmail.com>
2025-01-02 15:16:36 +0700

    tri: mengatur margin bottom pada heading1

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="text-center">
-      <h1 className="font-bold">Selamat Datang di Katalog Motor</h1>
+      <h1 className="font-bold mb-4">Selamat Datang di Katalog Motor</h1>
       <p>Temukan Motor Impian Anda</p>
     </div>
   );
