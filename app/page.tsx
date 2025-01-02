60d0d1c94bc45f769d217fdafc0eef56ce6fe37a
Tri Sasongko <tugasspakun@gmail.com>
2025-01-02 15:10:15 +0700

    tri: menambahkan deskripsi website dibawah judul

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <div className="text-center">
       <h1 className="font-bold">Selamat Datang di Katalog Motor</h1>
+      <p>Temukan Motor Impian Anda</p>
     </div>
   );
 }
