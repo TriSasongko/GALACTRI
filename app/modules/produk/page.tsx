1fa2787ec72cf6fdfa7391a43e0c76795c981dc2
Ludfi Eric Fernando <ericludfi19@gmail.com>
2025-01-03 21:23:32 +0700

    Eric:menambahkan data produk kedalam komponen ProdukPage

---
+++ app/modules/produk/page.tsx
@@ -0,0 +1,22 @@
+export default function ProdukPage() {
+  const products = [
+    {
+      id: 1,
+      name: "PCX",
+      price: "Rp 15.000.000",
+      image: "/PCX.png",
+    },
+    {
+      id: 2,
+      name: "Kawasaki",
+      price: "Rp 18.000.000",
+      image: "/kws650.png",
+    },
+    {
+      id: 3,
+      name: "Vario",
+      price: "Rp 20.000.000",
+      image: "/vario.png",
+    },
+  ];
+}
