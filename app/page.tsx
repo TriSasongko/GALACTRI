31b58ba4fbd8f26b97c857225d509e71ae79fe6b
Tri Sasongko <trisasongko010@gmail.com>
2025-01-06 03:29:41 +0700

    tri: menambahkan input field untuk fitur search motor

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,13 @@ export default function Home() {
       <h1 className="font-bold mb-4">Selamat Datang di Katalog Motor</h1>
       <p className="mb-6">Temukan Motor Impian Anda</p>
 
+      <div className="mb-6">
+        <input
+          type="text"
+          placeholder="Cari Motor..."
+          className="border border-gray-300 rounded-lg px-4 py-2 w-1/2"/>
+      </div>
+
       <div className="flex justify-center gap-4">
         <button className="bg-blue-500 text-white px-6 py-2 rounded-lg hover:bg-blue-600">Sport</button>
         <button className="bg-blue-500 text-white px-6 py-2 rounded-lg hover:bg-blue-600">Matic</button>
