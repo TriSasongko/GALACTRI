9daa52076b94dd8291e5be86cdb64cb4b4c0f490
Tri Sasongko <trisasongko010@gmail.com>
2025-01-06 02:14:33 +0700

    tri: membuat tombol untuk kategori jenis motor

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,15 @@ export default function Home() {
     <div className="text-center">
       <h1 className="font-bold mb-4">Selamat Datang di Katalog Motor</h1>
       <p className="mb-6">Temukan Motor Impian Anda</p>
+
+      <div>
+        <button></button>
+        <button className="bg-blue-500 text-white">Sport</button>
+        <button className="bg-blue-500 text-white">Matic</button>
+        <button className="bg-blue-500 text-white">Trail</button>
+      </div>
+
     </div>
+    
   );
 }
