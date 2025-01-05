c12636b592296c14e17ec1e4239a74a6d128453d
Tri Sasongko <trisasongko010@gmail.com>
2025-01-06 03:40:35 +0700

    tri: membuat tombol search untuk melakukan pencarian motor

--- app/page.tsx
+++ app/page.tsx
@@ -8,16 +8,24 @@ export default function Home() {
         <input
           type="text"
           placeholder="Cari Motor..."
-          className="border border-gray-300 rounded-lg px-4 py-2 w-1/2"/>
+          className="border border-gray-300 rounded-lg px-4 py-2 w-1/2"
+        />
+        <button className="bg-blue-500 text-white px-4 py-2 ml-2 rounded-lg hover:bg-blue-600">
+          Search
+        </button>
       </div>
 
       <div className="flex justify-center gap-4">
-        <button className="bg-blue-500 text-white px-6 py-2 rounded-lg hover:bg-blue-600">Sport</button>
-        <button className="bg-blue-500 text-white px-6 py-2 rounded-lg hover:bg-blue-600">Matic</button>
-        <button className="bg-blue-500 text-white px-6 py-2 rounded-lg hover:bg-blue-600">Trail</button>
+        <button className="bg-blue-500 text-white px-6 py-2 rounded-lg hover:bg-blue-600">
+          Sport
+        </button>
+        <button className="bg-blue-500 text-white px-6 py-2 rounded-lg hover:bg-blue-600">
+          Matic
+        </button>
+        <button className="bg-blue-500 text-white px-6 py-2 rounded-lg hover:bg-blue-600">
+          Trail
+        </button>
       </div>
-
     </div>
-    
   );
 }
