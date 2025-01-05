7c479a0d6ed82b6d1ee50f0ad639732358a0c9d7
GaluhMahesaPutra <galuhmahesa2003putra@gmail.com>
2025-01-05 22:38:20 +0700

    update dokumentasi

---
+++ app/modules/about/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AboutPage = () => {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      {/* Hero Section */}
+      <div className="relative">
+        <img
+          src="/ggg.jpg"
+          alt="Hero Image"
+          className="w-full h-90 object-cover"
+        />
+        
