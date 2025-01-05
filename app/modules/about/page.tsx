7f2afd08d6db4b9506fef01d2bdaeb1111ee0479
GaluhMahesaPutra <galuhmahesa2003putra@gmail.com>
2025-01-05 23:46:04 +0700

    Menambahkan judul catalog motor

--- app/modules/about/page.tsx
+++ app/modules/about/page.tsx
@@ -20,6 +20,11 @@ const AboutPage = () => {
        {/* Area Konten */}
        <div className="py-12 px-4 sm:px-6 lg:px-8">
         <div className="max-w-7xl mx-auto">
+        <div className="mt-12">
+            <h2 className="text-3xl font-bold text-gray-900">Tentang Catalog Motor</h2>
+            
+            </div>
+            
             </div>
             </div>
       </div>
