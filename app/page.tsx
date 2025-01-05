d6262c4fd56107db9a0848ce199b9239d0f699c1
Tri Sasongko <trisasongko010@gmail.com>
2025-01-06 02:28:21 +0700

    tri: mengatur tata letak button menggunakan flexbox

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
       <h1 className="font-bold mb-4">Selamat Datang di Katalog Motor</h1>
       <p className="mb-6">Temukan Motor Impian Anda</p>
 
-      <div>
+      <div className="flex justify-center gap-4">
         <button></button>
         <button className="bg-blue-500 text-white">Sport</button>
         <button className="bg-blue-500 text-white">Matic</button>
