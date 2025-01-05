fdf96f324a6c9a1dcf0a9e77dab86f1d6aed21d3
GaluhMahesaPutra <galuhmahesa2003putra@gmail.com>
2025-01-05 22:41:24 +0700

    menambahkan gambar

--- app/modules/about/page.tsx
+++ app/modules/about/page.tsx
@@ -10,4 +10,10 @@ const AboutPage = () => {
           alt="Hero Image"
           className="w-full h-90 object-cover"
         />
+         <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+          <h1 className="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl">
+            TENTANG KAMI
+          </h1>
+        </div>
+      </div>
         
