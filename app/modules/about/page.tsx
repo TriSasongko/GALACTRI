8dd9de23b7daf620fbb1b372c1aaab21506f0b67
GaluhMahesaPutra <galuhmahesa2003putra@gmail.com>
2025-01-05 23:12:50 +0700

    Menambahkan padding dan lebar maksimum untuk konten

--- app/modules/about/page.tsx
+++ app/modules/about/page.tsx
@@ -16,6 +16,12 @@ const AboutPage = () => {
           </h1>
         </div>
       </div>
+
+       {/* Area Konten */}
+       <div className="py-12 px-4 sm:px-6 lg:px-8">
+        <div className="max-w-7xl mx-auto">
+            </div>
+            </div>
       </div>
   );
 };
