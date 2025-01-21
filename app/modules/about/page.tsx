cc3d2f6775ffafe1ca7f24bb775f9621828a3bc9
GaluhMahesaPutra <galuhmahesa2003putra@gmail.com>
2025-01-21 17:32:14 +0700

    Menambahkan layout grid responsif untuk konten

--- app/modules/about/page.tsx
+++ app/modules/about/page.tsx
@@ -192,7 +192,15 @@ const AboutPage = () => {
     </p>
   </div>
 
-  
+   <div className="relative max-w-6xl mx-auto px-6 lg:px-12">
+   <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+
+    
+
+
+   </div>
+   </div>         
+
 
       </div>
 
